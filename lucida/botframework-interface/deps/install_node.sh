2d42761f15e61126e771194c2d07b4358a5646dc
Andrew Konwinski <andy@databricks.com>
2019-05-30 16:23:25 -0700

    upgrade node to v10

--- lucida/botframework-interface/deps/install_node.sh
+++ lucida/botframework-interface/deps/install_node.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# To get a version of npm that doesn't throw a "Error: CERT_UNTRUSTED"
+# we first need to update the system package source list for npm per
+# https://wellingguzman.com/notes/upgrading-nodejs-npm-on-ubuntu-14-04
+# and https://stackoverflow.com/questions/21855035/ssl-error-cert-untrusted-while-using-npm-command
+
+# Update the system package source list for npm so we will get node.js v10.
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+# now install npm
 sudo apt-get install npm && sudo npm install -g n && sudo n lts
